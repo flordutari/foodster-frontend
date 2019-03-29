7295841483643086bd1fb4d1e4748ed52b92bf4d
Flor <fdutari2@gmail.com>
2019-03-29 11:55:51 +0100

    first deploy adjust

--- src/lib/tupper-service.js
+++ src/lib/tupper-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class TupperService {
   constructor() {
     this.api = axios.create({
-      baseURL: 'http://localhost:5000/api/tuppers',
+      baseURL: process.env.REACT_APP_BACKEND_URL_TUPPERS,
       withCredentials: true
     });
   }
